8d4a42b5ff45fa75b686a74d60fc531cde922c45
Esteban Rodríguez Alonso <esrodriguezalo@gmail.com>
2020-09-30 22:31:00 -0500

    Added comments

--- Net-Core-API/Controllers/StudentsController.cs
+++ Net-Core-API/Controllers/StudentsController.cs
@@ -17,18 +17,36 @@ namespace Net_Core_API.Controllers
     {
         private StudentServices studentServices = new StudentServices();
 
+        /// <summary>
+        /// Retrieves all registered students
+        /// </summary>
+        /// <response code="200">Successful query</response>
+        /// <response code="400">Student has missing/invalid values</response>
+        /// <response code="500">Something went wrong</response>
         [HttpGet]
         public ActionResult<List<Student>> GetAllStudents()
         {
             return studentServices.GetAllStudents();
         }
 
+        /// <summary>
+        /// Retrieves an especific student
+        /// </summary>
+        /// <response code="200">Successful query</response>
+        /// <response code="400">Student has missing/invalid values</response>
+        /// <response code="500">Something went wrong searching the stundent</response>
         [HttpGet("{id}")]
         public ActionResult<Student> GetStudentById(int id)
         {
             return studentServices.GetStudent(id);
         }
 
+        /// <summary>
+        /// Add a student to the system
+        /// </summary>
+        /// <response code="200">Student added correctly</response>
+        /// <response code="400">Student has missing/invalid values</response>
+        /// <response code="500">Something went wrong adding the student</response>
         [HttpPost]
         public ActionResult<Student> AddStudent([FromBody] Student student)
         {
@@ -36,6 +54,12 @@ namespace Net_Core_API.Controllers
             return student;
         }
 
+        /// <summary>
+        /// Edit the information of an especific student
+        /// </summary>
+        /// <response code="200">Student edited correctly</response>
+        /// <response code="400">Student has missing/invalid values</response>
+        /// <response code="500">Something went wrong editing the student</response>
         [HttpPut]
         public ActionResult<Student> EditStudent([FromBody] Student student)
         {
@@ -43,6 +67,12 @@ namespace Net_Core_API.Controllers
             return student;
         }
 
+        /// <summary>
+        /// Deletes an especific student
+        /// </summary>
+        /// <response code="200">Student deleted correctly</response>
+        /// <response code="400">Student has missing/invalid values</response>
+        /// <response code="500">Something went wrong deleting the student</response>
         [HttpDelete]
         public ActionResult<Student> DeleteStudent([FromBody] Student student)
         {
--- Net-Core-API/Models/Student.cs
+++ Net-Core-API/Models/Student.cs
@@ -4,16 +4,30 @@ namespace Net_Core_API.Context
     public class Student
     {
         [Key]
+        /// <summary>
+        /// Student Id
+        /// </summary>
         public int Id { get; set; }
-
+        /// <summary>
+        /// Student user name
+        /// </summary>
         public string UserName { get; set; }
-
+        /// <summary>
+        /// Student first name
+        /// </summary>
+        /// <example>FirstNameLastName</example>
         public string FirstName { get; set; }
-
+        /// <summary>
+        /// Student last name
+        /// </summary>
         public string LastName { get; set; }
-
+        /// <summary>
+        /// Student age
+        /// </summary>
         public int Age { get; set; }
-
+        /// <summary>
+        /// Student career
+        /// </summary>
         public string Career { get; set; }
     }
 }
